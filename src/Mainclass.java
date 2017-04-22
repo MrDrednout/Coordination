fe08cddef88176465e60c08b31ac79f83682ce2d
MrDrednout_Games <coolezhik@yandex.ru>
2017-04-22 13:02:34 +0300

    Реализована главная экранная форма

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -10,7 +10,8 @@ public class Mainclass extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("windows/ruler/fxml/Ruler.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 200));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
